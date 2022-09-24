3650660f3c8c0fdcd6367ced4c6914b3cd0b8320
otradamax <otradamax@mail.ru>
2022-09-25 00:05:17 +0700

    Drawing blocks movement

--- AdaptiveCourseClient/MainWindow.xaml.cs
+++ AdaptiveCourseClient/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AdaptiveCourseClient.RenderObjects;
 
 namespace AdaptiveCourseClient
 {
@@ -20,9 +11,51 @@ namespace AdaptiveCourseClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private UIElement? logicElement;
+        private Point logicElementOffset;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 3; i++)
+            {
+                AddLogicElement();
+            }
         }
+
+        private void AddLogicElement()
+        {
+            Path logicElement = AND.Add();
+            logicElement.PreviewMouseLeftButtonDown += LogicElement_PreviewMouseLeftButtonDown;
+            logicElement.PreviewMouseMove += LogicElement_PreviewMouseMove;
+            logicElement.PreviewMouseLeftButtonUp += LogicElement_PreviewMouseLeftButtonUp;
+            sideBar.Children.Add(logicElement);
+        }
+
+        private void LogicElement_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            logicElement = (UIElement)sender;
+            logicElementOffset = e.GetPosition(sideBar);
+            logicElementOffset.Y -= Canvas.GetTop(logicElement);
+            logicElementOffset.X -= Canvas.GetLeft(logicElement);
+        }
+
+        private void LogicElement_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (logicElement == null)
+                return;
+
+            // Logic element movement
+            Point cursorPosition = e.GetPosition(sender as Canvas);
+            Canvas.SetTop(sender as UIElement, cursorPosition.Y - logicElementOffset.Y);
+            Canvas.SetLeft(sender as UIElement, cursorPosition.X - logicElementOffset.X);
+        }
+
+        private void LogicElement_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            logicElement = null;
+        }
+
     }
 }
---
+++ AdaptiveCourseClient/RenderObjects/AND.cs
@@ -0,0 +1,57 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows;
+using System;
+using System.Windows.Shapes;
+
+namespace AdaptiveCourseClient.RenderObjects
+{
+    public class AND
+    {
+        public static Path Add()
+        {
+            Path elementAND = new Path();
+            GeometryGroup geometryGroup = new GeometryGroup();
+            geometryGroup.FillRule = FillRule.Nonzero;
+            Canvas.SetTop(elementAND, 50);
+            Canvas.SetLeft(elementAND, 50);
+
+            RectangleGeometry rectanleBody = new RectangleGeometry();
+            ImageBrush imgBrush = new ImageBrush();
+            imgBrush.ImageSource = new BitmapImage(new Uri(@"../../../../images/and.png", UriKind.Relative));
+            rectanleBody.RadiusX = 3;
+            rectanleBody.RadiusY = 3;
+            rectanleBody.Rect = new Rect(0, 0, 50, 70);
+
+            int circleRadius = 5;
+
+            EllipseGeometry leftFirstCircle = new EllipseGeometry();
+            leftFirstCircle.RadiusX = circleRadius;
+            leftFirstCircle.RadiusY = circleRadius;
+            leftFirstCircle.Center = new Point(0, 20);
+
+            EllipseGeometry leftSecondCircle = new EllipseGeometry();
+            leftSecondCircle.RadiusX = circleRadius;
+            leftSecondCircle.RadiusY = circleRadius;
+            leftSecondCircle.Center = new Point(0, 50);
+
+            EllipseGeometry rightCircle = new EllipseGeometry();
+            rightCircle.RadiusX = circleRadius;
+            rightCircle.RadiusY = circleRadius;
+            rightCircle.Center = new Point(50, 35);
+
+            geometryGroup.Children.Add(rectanleBody);
+            geometryGroup.Children.Add(leftFirstCircle);
+            geometryGroup.Children.Add(leftSecondCircle);
+            geometryGroup.Children.Add(rightCircle);
+
+            elementAND.Data = geometryGroup;
+            elementAND.Fill = imgBrush;
+            elementAND.Stroke = Brushes.Black;
+            elementAND.StrokeThickness = 3;
+
+            return elementAND;
+        }
+    }
+}
