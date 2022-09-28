209e60b5f2b69bae613c6a3e79c258333dbdcb97
otradamax <otradamax@mail.ru>
2022-09-29 00:33:23 +0700

    success attempt of moving multiple uielemnts as a single element

--- AdaptiveCourseClient/MainWindow.xaml.cs
+++ AdaptiveCourseClient/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 using AdaptiveCourseClient.RenderObjects;
 
 namespace AdaptiveCourseClient
@@ -14,78 +16,76 @@ namespace AdaptiveCourseClient
     public partial class MainWindow : Window
     {
         private UIElement? logicElement;
+        private List<UIElement> logicElements = new List<UIElement>();
         private Point logicElementOffset;
-        private List<UIElement> list = new List<UIElement>();
+        private List<List<UIElement>> list = new List<List<UIElement>>();
 
 
         public MainWindow()
         {
             InitializeComponent();
 
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 3; i++)
             {
-                list = ElementAND.AddAND(LogicElementAND_PreviewMouseLeftButtonDown, bodyCanvas);
+                list.Add(ElementAND.AddAND(LogicElementAND_PreviewMouseLeftButtonDown, LogicElement_PreviewMouseMove,
+                    LogicElement_PreviewMouseLeftButtonUp, bodyCanvas));
             }
-        }
-
-        private void AddLogicElement()
-        {
-            Path logicElement = AND.Add();
-            logicElement.PreviewMouseLeftButtonDown += LogicElement_PreviewMouseLeftButtonDown;
-            logicElement.PreviewMouseMove += LogicElement_PreviewMouseMove;
-            logicElement.PreviewMouseLeftButtonUp += LogicElement_PreviewMouseLeftButtonUp;
-            bodyCanvas.Children.Add(logicElement);
         }
 
         private void LogicElementAND_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             logicElement = (UIElement)sender;
-            if (list.Contains(logicElement))
+            foreach(List<UIElement> logicBlock in list)
             {
-                List<UIElement> listFinal = list.FindAll(t => t== logicElement);
+                if (logicBlock.Contains(logicElement))
+                {
+                    logicElements = logicBlock;
+                }
             }
             logicElementOffset = e.GetPosition(bodyCanvas);
             logicElementOffset.Y -= Canvas.GetTop(logicElement);
             logicElementOffset.X -= Canvas.GetLeft(logicElement);
-            Panel.SetZIndex(logicElement, 1);
-            logicElement.CaptureMouse();
-        }
-
-        private void LogicElement_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            logicElement = (UIElement)sender;
-            logicElementOffset = e.GetPosition(bodyCanvas);
-            logicElementOffset.Y -= Canvas.GetTop(logicElement);
-            logicElementOffset.X -= Canvas.GetLeft(logicElement);
-            Panel.SetZIndex(logicElement, 1);
+            //Panel.SetZIndex(logicElement, 1);
             logicElement.CaptureMouse();
         }
 
         private void LogicElement_PreviewMouseMove(object sender, MouseEventArgs e)
         {
-            if (logicElement == null)
+            if (logicElements == null || logicElement == null)
                 return;
 
             // Logic element movement
             Point cursorPosition = e.GetPosition(sender as Canvas);
-            Canvas.SetTop(logicElement, cursorPosition.Y - logicElementOffset.Y);
-            Canvas.SetLeft(logicElement, cursorPosition.X - logicElementOffset.X);
+
+            double positionXMain = Canvas.GetLeft(logicElements[0]);
+            double positionYMain = Canvas.GetTop(logicElements[0]);
+
+            foreach (UIElement uIElement in logicElements)
+            {
+                double positionX = Canvas.GetLeft(uIElement);
+                double positionY = Canvas.GetTop(uIElement);
+                double Y = cursorPosition.Y - logicElementOffset.Y - (positionYMain - positionY);
+                double X = cursorPosition.X - logicElementOffset.X - (positionXMain - positionX);
+                Canvas.SetTop(uIElement, Y);
+                Canvas.SetLeft(uIElement, X);
+            }
         }
 
         private void LogicElement_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (logicElement == null)
+            if (logicElements == null)
                 return;
 
-            Point position = e.GetPosition(sender as Canvas);
-            if (position.X < Toolbox.ActualWidth)
-            {
-                Canvas.SetTop(logicElement, 50);
-                Canvas.SetLeft(logicElement, 50);
-            }
-            Panel.SetZIndex(logicElement, 0);
+            //Point position = e.GetPosition(sender as Canvas);
+            //if (position.X < Toolbox.ActualWidth)
+            //{
+            //    Canvas.SetTop(logicElement, 50);
+            //    Canvas.SetLeft(logicElement, 50);
+            //}
+            //Panel.SetZIndex(logicElement, 0);
             logicElement?.ReleaseMouseCapture();
             logicElement = null;
+            logicElements = null;
         }
     }
 }
--- AdaptiveCourseClient/RenderObjects/ElementAND.cs
+++ AdaptiveCourseClient/RenderObjects/ElementAND.cs
@@ -9,10 +9,19 @@ namespace AdaptiveCourseClient.RenderObjects
 {
     public class ElementAND
     {
-        public static List<UIElement> AddAND(MouseButtonEventHandler LogicElementAND_PreviewMouseLeftButtonDown, Canvas canvas)
+        public static List<UIElement> AddAND(MouseButtonEventHandler LogicElementAND_PreviewMouseLeftButtonDown,
+            MouseEventHandler LogicElement_PreviewMouseMove, MouseButtonEventHandler LogicElement_PreviewMouseLeftButtonUp,  Canvas canvas)
         {
             List<UIElement> list = new List<UIElement>();
 
+            Rectangle rectangle = AddRectangle();
+            Canvas.SetLeft(rectangle, 50);
+            Canvas.SetTop(rectangle, 50);
+            rectangle.PreviewMouseLeftButtonDown += LogicElementAND_PreviewMouseLeftButtonDown;
+            rectangle.PreviewMouseMove += LogicElement_PreviewMouseMove;
+            rectangle.PreviewMouseLeftButtonUp += LogicElement_PreviewMouseLeftButtonUp;
+            list.Add(rectangle);
+            canvas.Children.Add(rectangle);
             Ellipse leftFirstCircle = AddCircle();
             Canvas.SetLeft(leftFirstCircle, 44);
             Canvas.SetTop(leftFirstCircle, 60);
@@ -20,33 +29,25 @@ namespace AdaptiveCourseClient.RenderObjects
             canvas.Children.Add(leftFirstCircle);
             Ellipse leftSecondCircle = AddCircle();
             Canvas.SetLeft(leftSecondCircle, 44);
-            Canvas.SetTop(leftSecondCircle, 85);
-            list.Add(leftFirstCircle);
+            Canvas.SetTop(leftSecondCircle, 95);
+            list.Add(leftSecondCircle);
             canvas.Children.Add(leftSecondCircle);
             Ellipse rightCircle = AddCircle();
-            Canvas.SetLeft(rightCircle, 95);
-            Canvas.SetTop(rightCircle, 75);
-            list.Add(leftFirstCircle);
+            Canvas.SetLeft(rightCircle, 90);
+            Canvas.SetTop(rightCircle, 80);
+            list.Add(rightCircle);
             canvas.Children.Add(rightCircle);
-            Rectangle rectangle = AddRectangle();
-            Canvas.SetLeft(rectangle, 50);
-            Canvas.SetTop(rectangle, 50);
-
-            rectangle.PreviewMouseLeftButtonDown += LogicElementAND_PreviewMouseLeftButtonDown;
-            list.Add(rectangle);
-            canvas.Children.Add(rectangle);
-
             return list;
         }
 
         private static Ellipse AddCircle()
         {
             Ellipse Circle = new Ellipse();
-            Circle.Width = 10;
-            Circle.Height = 10;
+            Circle.Width = 15;
+            Circle.Height = 15;
             Circle.Fill = Brushes.White;
             Circle.Stroke = Brushes.Black;
-            Circle.StrokeThickness = 2;
+            Circle.StrokeThickness = 3;
             return Circle;
         }
 
